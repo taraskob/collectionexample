aa6edaf9641a932acc4d1e85961036d1000472d3
Taras Kobevko <taraskob@yahoo.com>
2017-03-28 18:59:12 +0300

    mode

--- CollectionMain.java
+++ CollectionMain.java
@@ -3,10 +3,10 @@ import java.util.*;
 
 class CollectionMain {
     public static void main(String[] args) {
-        fillTreeMap(CollectionTest.getDirList(),CollectionTest.getFileList());
+        fillTreeMap(CollectionTest.getDirList(), CollectionTest.getFileList());
     }
 
-    static TreeMap<String, TreeSet> fillTreeMap(ArrayList<String> dirList,ArrayList<String> fileList) {
+    static TreeMap<String, TreeSet> fillTreeMap(ArrayList<String> dirList, ArrayList<String> fileList) {
         TreeSet<String> filesTreeSet = new TreeSet<>(fileList);
         TreeMap<String, TreeSet> dirsTreeMap = new TreeMap<>();
         ArrayList<String> dirsList = dirList;
@@ -14,13 +14,12 @@ class CollectionMain {
             SortedSet<String> ss_approx = filesTreeSet.subSet(dirName + '\\', dirName + '\\' + Character.MAX_VALUE);
             TreeSet ss_exact = new TreeSet();
             String low = dirName + '\\';
+
             for (String fileName : ss_approx) {
                 if (low.lastIndexOf('\\') == fileName.lastIndexOf('\\'))
                     ss_exact.add(fileName);
-                else {
-                    dirsTreeMap.put(dirName, new TreeSet(ss_exact));
-                }
-            }
+                           }
+
             dirsTreeMap.put(dirName, new TreeSet(ss_exact));
         }
         return dirsTreeMap;
